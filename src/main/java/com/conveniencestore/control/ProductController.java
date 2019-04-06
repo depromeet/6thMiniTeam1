0afd3f94cb51aa135aa1b1f0b1c53fc1fc150eea
cjh5414 <cjh5414@gmail.com>
2019-04-06 17:43:53 +0900

    Setup jpa

--- src/main/java/com/conveniencestore/control/ProductController.java
+++ src/main/java/com/conveniencestore/control/ProductController.java
@@ -4,23 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.conveniencestore.dao.ProductDao;
 import com.conveniencestore.model.Product;
 
 @RestController
-@RequestMapping("/product")
 public class ProductController {
 
 	@Autowired
 	private ProductDao productDao;
 
 	@GetMapping("/products")
-	public List<Product> getproducts(){
+	public List<Product> getProducts(@RequestParam(value="page", required=false) Integer page, @RequestParam(value="count", required=false) Integer count) {
 		List<Product> products = productDao.findAll();
+		if (page == null) page = 1;
+		if (count == null) count = 30;
+
+		System.out.println(page);
+		System.out.println(count);
+
 		return products;
 	}
-
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert into product (seq, name, storename, price, event, imageurl, createtime) values (1, 'pr1', 'cu', 1000, '1+1', 'www.google.com', '2019-01-02 11:20:14.000');
